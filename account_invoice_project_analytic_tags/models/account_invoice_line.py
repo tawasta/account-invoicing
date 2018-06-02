0fbff8827c584ccb0550b3b8065443ce8f6fd3fc
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-02 20:30:19 +0300

    Add a new module for adding analytic tags from invoice line analytic id

---
+++ account_invoice_project_analytic_tags/models/account_invoice_line.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class AccountInvoiceLine(models.Model):
+
+    _inherit = 'account.invoice.line'
+
+    @api.onchange('account_analytic_id')
+    def onchange_analytic_account_id_update_analytic_tags(self):
+        for record in self:
+            if record.account_analytic_id \
+                    and record.account_analytic_id.tag_ids:
+                record.analytic_tag_ids = record.account_analytic_id.tag_ids
