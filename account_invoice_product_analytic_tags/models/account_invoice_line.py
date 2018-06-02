39323c543eb25a6a83927aa2491fe298e62ecb9b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-02 20:21:12 +0300

    Improve readme

---
+++ account_invoice_product_analytic_tags/models/account_invoice_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class AccountInvoiceLine(models.Model):
+
+    _inherit = 'account.invoice.line'
+
+    @api.onchange('product_id')
+    def onchange_product_id_update_analytic_tags(self):
+        for record in self:
+            if record.product_id and record.product_id.get_analytic_tags():
+                record.analytic_tag_ids = record.product_id.get_analytic_tags()
