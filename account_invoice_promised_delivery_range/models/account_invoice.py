07816063c6a112c085a432bd624977939d6cb29a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-07 16:30:26 +0300

    Add new module for delivery range on invoice

---
+++ account_invoice_promised_delivery_range/models/account_invoice.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'account.invoice'
+
+    date_delivery_promised_start = fields.Date(
+        string='Promised Delivery start',
+        readonly=True,
+        states={
+            'draft': [('readonly', False)],
+            'sent': [('readonly', False)]
+        },
+        copy=False,
+    )
+
+    date_delivery_promised_end = fields.Date(
+        string='Promised Delivery end',
+        readonly=True,
+        states={
+            'draft': [('readonly', False)],
+            'sent': [('readonly', False)]
+        },
+        copy=False,
+    )
