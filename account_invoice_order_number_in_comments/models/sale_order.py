a70c67db5fa35d20e0ad29e2276574fd746a5e7a
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-06-07 10:38:32 +0300

    New module account_invoice_order_number_in_comments
    New module for storing the SO number in the resulting
    invoice's Terms and Conditions freetext field

---
+++ account_invoice_order_number_in_comments/models/sale_order.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _prepare_invoice(self):
+        # Handling for when invoicing the invoiceable lines
+        self.ensure_one()
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+
+        if not invoice_vals['comment']:
+            invoice_vals['comment'] = ''
+
+        invoice_vals['comment'] += \
+            u'\n%s: %s' % (_('Our Order Reference'), self.name)
+
+        return invoice_vals
