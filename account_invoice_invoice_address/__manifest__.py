fd5e1cd9fb0fa7e1abc460c167212fd645246f4e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-07-26 15:24:32 +0300

    Add default context for invoice address

--- account_invoice_invoice_address/__manifest__.py
+++ account_invoice_invoice_address/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Separate Invoice Address for Invoices',
     'summary': "Separates partner and invoice address fields",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Invoicing',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
