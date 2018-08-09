794fb13921565fda484a1b64ee455d2eee4c6c37
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-08-09 16:38:55 +0300

    Add a new module for forcing the delivery and invoicing address to be children of the main partner

---
+++ account_invoice_partner_strict/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2018 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': 'Strict domain filters for partners on invoice',
+    'summary': 'Only allow correct type and parent for invoice addresses',
+    'category': 'Invoicing',
+    'version': '10.0.1.0.0',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account',
+        'account_invoice_invoice_address',
+    ],
+    'data': [
+        'views/account_invoice.xml',
+    ],
+}
