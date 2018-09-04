754e669a20a72413db00286be844394633988a97
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-09-04 12:53:45 +0300

    New module account_invoice_circulation_mandatory_analytic

---
+++ account_invoice_circulation_mandatory_analytic/models/account_invoice.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = 'account.invoice'
+
+    def action_invoice_approve(self):
+        print "ovrride"
+        if not all(line.account_analytic_id for line in self.invoice_line_ids):
+            msg = \
+                _('Please add analytic accounts to all lines before approving')
+            raise UserError(msg)
+
+        return super(AccountInvoice, self).action_invoice_approve()
