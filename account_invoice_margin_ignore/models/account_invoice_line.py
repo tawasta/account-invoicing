68eba1968a1c68f1f36068a928f9f2798a9b32c6
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-10-16 19:58:41 +0300

    New module account_invoice_margin_ignore

---
+++ account_invoice_margin_ignore/models/account_invoice_line.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models
+
+
+class AccountInvoiceLine(models.Model):
+
+    _inherit = 'account.invoice.line'
+
+    @api.depends('product_id', 'purchase_price', 'quantity',
+                 'price_unit', 'price_subtotal')
+    def _product_margin(self):
+
+        super(AccountInvoiceLine, self)._product_margin()
+
+        for line in self:
+            if line.product_id.margin_ignore:
+                line.margin = 0
