2ed06169ce08b0875cbba79b7337d715dbdf1db0
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-10-16 19:50:59 +0300

    New module account_invoice_margin

---
+++ account_invoice_margin/models/account_invoice.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+import odoo.addons.decimal_precision as dp
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = "account.invoice"
+
+    margin = fields.Monetary(
+        compute='_product_margin',
+        help=_('It gives profitability by calculating the difference between '
+               'the Unit Price and the cost.'),
+        currency_field='currency_id',
+        digits=dp.get_precision('Product Price'),
+        store=True)
+
+    @api.depends('invoice_line_ids.margin')
+    def _product_margin(self):
+        for invoice in self:
+            margin = sum(invoice.invoice_line_ids.filtered(
+                lambda r: r.invoice_id.state != 'cancel').mapped('margin'))
+            sign = invoice.type in ['in_refund', 'out_refund'] and -1 or 1
+            invoice.margin = sign * margin
