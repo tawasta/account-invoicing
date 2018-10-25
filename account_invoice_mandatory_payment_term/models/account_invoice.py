e19e405ac7a8c893d5d8b2bab9778eab4aec34ec
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-10-25 09:28:34 +0300

    Add a new module for setting payment term as mandatory

---
+++ account_invoice_mandatory_payment_term/models/account_invoice.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def invoice_validate(self):
+        self._check_invoice_payment_term()
+
+        return super(AccountInvoice, self).invoice_validate()
+
+    def _check_invoice_payment_term(self):
+        for record in self:
+            raise UserError(_('Please set a payment term before validating'))
