e5822539af5ef836111d7a09cfffb6a199c8c622
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2019-12-11 14:55:17 +0200

    10.0 account invoice default open (#33)
    * [ADD] New module for showing open and draft invoices by default
    
    * [ADD] Support for default customer invoices

--- account_invoice_default_open/__manifest__.py
+++ account_invoice_default_open/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Show only open and draft invoices by default',
     'summary': 'Show only open and draft invoices by default',
     'category': 'Invoicing',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
