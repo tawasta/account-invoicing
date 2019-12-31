02c44b006fe88f830992a130cd4b60075e18a8e7
difbit <timo.kekalainen@hotmail.com>
2019-12-31 15:29:23 +0200

    [ADD] Python file: sale.order-model

---
+++ account_invoice_salesperson_phone_and_email/models/sale_order.py
@@ -0,0 +1,33 @@
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _prepare_invoice(self):
+        """Assign phone and email values to Invoice's comment-field"""
+        vals = super(SaleOrder, self)._prepare_invoice()
+
+        phone, email = (self.user_id.partner_id.phone,
+                        self.user_id.partner_id.email)
+
+        note = vals['comment']
+
+        # Conditions to add newlines
+        email_line = '\n' if note or phone else ''
+        phone_line = '\n' if note else ''
+
+        vals['comment'] = "%s%s%s" % (
+            '%s' % note if note else '',
+            '%s%s' % (phone_line, phone) if phone else '',
+            '%s%s' % (email_line, email) if email else ''
+        )
+
+        # These values are used on account.invoice-model's onchange-method
+        vals['salesperson_phone'] = phone
+        vals['salesperson_email'] = email
+
+        return vals
