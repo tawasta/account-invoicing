cc3c48e0970a4e81b727dd5e33be5d3a99787572
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2020-01-08 09:57:55 +0200

    10.0 account invoice default my dependency fix (#41)
    * [REM] Obsolete dependency
    
    * [REM] Default states
    
    * [FIX] Default journal type

--- account_invoice_default_my/__manifest__.py
+++ account_invoice_default_my/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Show my invoices by default',
     'summary': 'Show invoices by default',
     'category': 'Invoicing',
-    'version': '10.0.1.3.0',
+    'version': '10.0.1.3.1',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
