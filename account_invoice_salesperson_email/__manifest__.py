1c01c48c55aba8ac0778294ad1f12c46d5e0782b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2020-01-28 13:10:50 +0200

    [FIX] Author

--- account_invoice_salesperson_email/__manifest__.py
+++ account_invoice_salesperson_email/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '12.0.1.0.0',
     'category': 'Invoicing',
     'website': 'https://tawasta.fi',
-    'author': 'Oy Tawasta Technologies Ltd.',
+    'author': 'Tawasta',
     'license': 'AGPL-3',
     'application': False,
     'installable': True,
