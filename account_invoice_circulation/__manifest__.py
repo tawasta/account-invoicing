c22a5f77aaf75f9ab2cae4eaa7c274d224bea69c
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-03-30 18:19:54 +0300

    [IMP] Hide approve buttons when editing

--- account_invoice_circulation/__manifest__.py
+++ account_invoice_circulation/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Invoice circulation',
     'summary': 'Adds invoice circulation for vendor invoices',
-    'version': '10.0.1.7.1',
+    'version': '10.0.1.7.2',
     'category': 'Invoicing',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
