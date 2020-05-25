a389ca4e97ae27513a44f55cbccd80a62651b5a4
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-05-25 08:55:50 +0300

    [FIX] Broken field references

--- account_invoice_notice_period/__manifest__.py
+++ account_invoice_notice_period/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Notice period for Invoices",
     "summary": "Notice period field for invoices and partners",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Invoicing",
     "website": "https://github.com/Tawasta/account-invoicing",
     "author": "Tawasta",
