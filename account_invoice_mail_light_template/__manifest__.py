18bfe663531d339e00534054f106f4a6e3764eee
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-06-24 13:12:40 +0300

    [FIX] The used model

--- account_invoice_mail_light_template/__manifest__.py
+++ account_invoice_mail_light_template/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Use light mail template for invoices",
     "summary": "Invoice emails use light mail template",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Invoicing",
     "website": "https://github.com/Tawasta/account-invoicing",
     "author": "Tawasta",
--- account_invoice_mail_light_template/wizard/account_invoice_send.py
+++ account_invoice_mail_light_template/wizard/account_invoice_send.py
@@ -2,11 +2,11 @@ from odoo import api
 from odoo import models
 
 
-class AccountInvoice(models.Model):
+class AccountInvoiceSend(models.TransientModel):
 
-    _inherit = "account.invoice"
+    _inherit = "account.invoice.send"
 
     @api.multi
     def _send_email(self):
         self = self.with_context(custom_layout='mail.mail_notification_light')
-        return super(AccountInvoice, self)._send_email()
+        return super(AccountInvoiceSend, self)._send_email()
