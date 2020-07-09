f12aa5d7330bd914435a62a5b5410e1d048fbc51
retropikzel <retropikzel@mailbox.org>
2020-07-09 13:57:13 +0300

    [FIX] Invoice line sales agent

--- account_invoice_line_sales_agent/models/account_invoice_line.py
+++ account_invoice_line_sales_agent/models/account_invoice_line.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
-    sales_agent = fields.Many2one(related="invoice_id.sales_agent", stored=True,)
+    sales_agent = fields.Many2one(related="invoice_id.sales_agent", store=True,)
