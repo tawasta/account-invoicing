aff53f162735158e32c183815cc897e24f75ff75
retropikzel <retropikzel@mailbox.org>
2020-07-09 08:38:56 +0300

    [FIX] Added temporary to attachment name

--- account_invoice_report_zip/models/account_invoice.py
+++ account_invoice_report_zip/models/account_invoice.py
@@ -1,8 +1,6 @@
 import base64
 import os
-import shutil
 import zipfile
-from random import randrange
 
 from odoo import api, models
 
@@ -13,45 +11,40 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def to_zip(self, records, archive_name):
-        archive_path = "/tmp/{}.{}".format(
-            archive_name,
-            "zip",
-        )
+        model_name = "account.account_invoices"
+        archive_path = "/tmp/{}.{}".format(archive_name, "zip",)
 
+        with zipfile.ZipFile(archive_path, "w") as zip_archive:
             for record in records:
-                file_name = "{} - {} - {}.{}".format(
-                    record._get_report_base_filename(),
-                    record.id,
-                    record.name,
-                    "pdf"
-                )
+                pdf_file = self.env.ref(model_name).sudo().render_qweb_pdf([record.id])
 
-                pdf_file = self.env.ref("account.account_invoices")\
-                    .sudo().render_qweb_pdf([record.id])
+                file_name = "{} - {} - {}.{}".format(
+                    record._get_report_base_filename(), record.id, record.name, "pdf"
+                )
 
                 zip_archive.writestr(
-                    file_name,
-                    data=pdf_file[0],
+                    file_name, data=pdf_file[0],
                 )
 
+        archive_filename = "{}.{}".format(archive_name, "zip",)
 
-        archive_filename = "{}.{}".format(
-            archive_name,
-            "zip",
+        f = open(archive_path, "rb")
+        archive_attachment = (
+            self.env["ir.attachment"]
+            .sudo()
+            .create(
+                {
+                    "name": "{}-{}".format("temporary", archive_filename),
+                    "public": False,
+                    "datas": base64.b64encode(f.read()),
+                    "type": "binary",
+                }
+            )
         )
-
-
-        f = open(archive_path, 'rb')
-        archive_attachment = self.env["ir.attachment"].sudo().create({
-            "name": archive_filename,
-            "display_name": archive_filename,
-            "res_name": archive_filename,
-            "public": False,
-            "datas_fname": archive_filename,
-            "datas": base64.b64encode(f.read()),
-            "type": "binary",
-        })
         f.close()
+
+        if os.path.isfile(archive_path):
+            os.remove(archive_path)
 
         return {
             "type": "ir.actions.act_url",
