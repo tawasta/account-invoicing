9a984b9770ee82d5a7f0c4cdc0e2b9c867c0d634
retropikzel <retropikzel@mailbox.org>
2020-07-13 15:11:20 +0300

    [ADD] New module

---
+++ account_invoice_line_sale_order_number/models/account_invoice_line.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    sale_order_ids = fields.One2many(related="invoice_id.sale_order_ids")
+
+    sale_order_numbers = fields.Char(compute="_compute_sale_order_numbers",)
+
+    def _compute_sale_order_numbers(self):
+        numbers = ""
+        for sale_order in self.sale_order_ids:
+            numbers = "{} {}".format(numbers, sale_order.name,)
+
+        print("============")
+        print(numbers)
+        print("============")
+        self.sale_order_numbers = numbers
