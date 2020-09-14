7435da5ddddc629e5f9ddd4bd1ad8ee5976eac34
difbit <timo.kekalainen@hotmail.com>
2020-09-14 16:18:53 +0300

    [IMP] Set store=True to enable sorting

--- account_invoice_line_sale_order_number/models/account_invoice_line.py
+++ account_invoice_line_sale_order_number/models/account_invoice_line.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
-    origin = fields.Char(related="invoice_id.origin")
+    origin = fields.Char(related="invoice_id.origin", store=True)
