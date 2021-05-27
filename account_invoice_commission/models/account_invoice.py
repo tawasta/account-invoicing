6c60d3ee0a8f19a6271605193f30c04da12f481e
difbit <timo.kekalainen@hotmail.com>
2021-05-27 14:43:24 +0300

    [MIG] Migration to 14.0

--- account_invoice_commission/models/account_invoice.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
-
-    commission_paid = fields.Boolean(string="Commission paid", default=False)
