78dd1246d0f76c7963ac23a00feda34cac70aad9
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-05-28 14:37:59 +0300

    [FIX] Margin ignore compute

--- account_invoice_margin_ignore/models/account_invoice_line.py
+++ account_invoice_margin_ignore/models/account_invoice_line.py
@@ -5,13 +5,12 @@ class AccountInvoiceLine(models.Model):
 
     _inherit = "account.invoice.line"
 
-    @api.depends(
-        "product_id", "purchase_price", "quantity", "price_unit", "price_subtotal"
-    )
-    def _compute_product_margin(self):
+    @api.depends("purchase_price", "price_subtotal")
+    def _compute_margin(self):
 
-        super(AccountInvoiceLine, self)._compute_product_margin()
+        super(AccountInvoiceLine, self)._compute_margin()
 
         for line in self:
             if line.product_id.margin_ignore:
                 line.margin = 0
+                line.margin_percent = 0
