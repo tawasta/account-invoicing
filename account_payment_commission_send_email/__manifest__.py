0d8b28092b230fcf43c24b0b013a818160e4aebe
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-03-29 12:56:24 +0300

    Created a new module

---
+++ account_payment_commission_send_email/__manifest__.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#    Author: Jarmo Kortetjärvi
+#    Copyright 2015 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Account payment commission send email",
+    "summary": "Account payment commission send email",
+    "version": "14.0.0.1.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/Tawasta/account-invoicing",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["account","account_invoice_commission_payment"],
+    "data": ["data/mail_template_data.xml","security/ir.model.access.csv","views/account_payment.xml", "wizard/account_payment_email.xml"],
+}
---
+++ account_payment_commission_send_email/wizard/account_payment_email.py
@@ -0,0 +1,145 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+import re
+import base64
+
+from odoo import _
+from odoo import api
+from odoo import fields
+from odoo import models
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+emails_split = re.compile(r"[;,\n\r]+")
+
+
+class AccountPaymentEmail(models.TransientModel):
+    _name = "account.payment.email"
+    _description = "Account payment email"
+
+    # composer content
+    subject = fields.Char(
+        "Subject", compute="_compute_subject", readonly=False, store=True
+    )
+    body = fields.Html(
+        "Contents",
+        sanitize_style=True,
+        compute="_compute_body",
+        readonly=False,
+        store=True,
+    )
+    attachment_ids = fields.Many2many("ir.attachment", string="Attachments", compute="_get_attachment", readonly=False)
+    template_id = fields.Many2one(
+        "mail.template",
+        "Use template",
+        domain="[('model', '=', 'account.payment')]",
+        readonly=True,
+    )
+    # recipients
+    partner_ids = fields.Many2many(
+        "res.partner", string="Recipients", compute="_get_recipients", readonly=False,
+    )
+
+    account_payment_id = fields.Many2one("account.payment", string="Account payment", required=True)
+
+
+    @api.depends("template_id")
+    def _compute_subject(self):
+        for record in self:
+            if record.template_id:
+                record.subject = record.template_id.subject
+            elif not record.subject:
+                record.subject = False
+
+    @api.depends("template_id")
+    def _get_attachment(self):
+        for record in self:
+            if record.template_id:
+                pdf = self.env.ref('account_invoice_commission_payment.action_report_payment_commissions')._render_qweb_pdf(record.account_payment_id.id)
+                b64_pdf = base64.b64encode(pdf[0])
+                name = "Payment settlements"
+                create_attachment = self.env['ir.attachment'].sudo().create({
+                    'name': name,
+                    'type': 'binary',
+                    'datas': b64_pdf,
+                    'store_fname': b64_pdf,
+                    'mimetype': 'application/x-pdf'
+                })
+
+                record.attachment_ids = create_attachment
+            elif not record.subject:
+                record.subject = False
+
+    @api.depends("template_id")
+    def _compute_body(self):
+        for record in self:
+            if record.template_id:
+                record.body = record.template_id.body_html
+            elif not record.body:
+                record.body = False
+
+    @api.depends("template_id")
+    def _get_recipients(self):
+        for record in self:
+            if record.template_id:
+                record.partner_ids = record.account_payment_id.partner_id.ids
+            elif not record.body:
+                record.body = False
+
+    def action_send(self):
+        self.ensure_one()
+
+        if not self.env.user.email:
+            raise UserError(
+                _(
+                    "Unable to post message, please configure the sender's email address."
+                )
+            )
+
+        payment_id = self.account_payment_id
+        mail_values = []
+        for partner in self.partner_ids:
+            mail_values.append(self._add_mail_values(payment_id, partner))
+
+        for mail_value in mail_values:
+            new_mail = self.env["mail.mail"].sudo().create(mail_value)
+            if new_mail:
+                new_mail.send()
+
+        return {"type": "ir.actions.act_window_close"}
+
+    def _add_mail_values(self, payment_id, partner):
+
+        subject = self.env["mail.render.mixin"]._render_template(
+            self.subject, "account.payment", payment_id.ids, post_process=True
+        )[payment_id.id]
+        body = self.env["mail.render.mixin"]._render_template(
+            self.body, "account.payment", payment_id.ids, post_process=True
+        )[payment_id.id]
+
+        mail_values = {
+            "email_from": self.env.user.email_formatted,
+            "author_id": self.env.user.partner_id.id,
+            "model": "account.payment",
+            "res_id": payment_id.id,
+            "subject": subject,
+            "body_html": body,
+            "attachment_ids": [(4, att.id) for att in self.attachment_ids],
+            "auto_delete": True,
+            "recipient_ids": [(4, partner.id)],
+        }
+        mail_values["body_html"] = self.env["mail.render.mixin"]._replace_local_links(
+            body
+        )
+
+        payment_id.sudo().message_post(
+            message_type="comment",
+            subtype_xmlid="mail.mt_note",
+            subject=subject,
+            body=body,
+            notify_by_email=False,
+            attachment_ids=self.attachment_ids.ids,
+        )
+
+        return mail_values
