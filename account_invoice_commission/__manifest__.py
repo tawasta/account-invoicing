16f086305ea92d53e749e5683c814d23957739e4
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-05-06 21:31:36 +0300

    [REF] Run pre-commit

--- account_invoice_commission/__manifest__.py
+++ account_invoice_commission/__manifest__.py
@@ -23,7 +23,7 @@
     "summary": "account_invoice_commission",
     "version": "14.0.1.0.2",
     "category": "Invoicing",
-    "website": "https://github.com/Tawasta/account-invoicing",
+    "website": "https://gitlab.com/Tawasta/odoo/account-invoicing",
     "author": "Tawasta",
     "license": "AGPL-3",
     "application": False,
