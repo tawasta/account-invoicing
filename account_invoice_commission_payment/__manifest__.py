4889406398dfee6a673ebe8a313145514efcf8d5
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-05-11 11:47:55 +0300

    [IMP] Mass operation performance

--- account_invoice_commission_payment/__manifest__.py
+++ account_invoice_commission_payment/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Account Invoice - Create commission payments from invoices",
     "summary": "Allows Making commission payments from invoices",
-    "version": "14.0.1.4.2",
+    "version": "14.0.1.4.3",
     "category": "Invoicing",
     "website": "https://gitlab.com/Tawasta/odoo/account-invoicing",
     "author": "Tawasta",
