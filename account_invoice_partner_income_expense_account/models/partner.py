4b8321037e75cc2d4379cfca46f541c80814736c
difbit <timo.kekalainen@hotmail.com>
2022-06-01 12:52:15 +0300

    [MIG] Migration to 14.0 version and updated translations

---
+++ account_invoice_partner_income_expense_account/models/partner.py
@@ -0,0 +1,27 @@
+from odoo import fields
+from odoo import models
+
+
+class Partner(models.Model):
+
+    _inherit = "res.partner"
+
+    property_account_income_id = fields.Many2one(
+        "account.account",
+        company_dependent=True,
+        string="Income account",
+        oldname="default_income_account",
+        domain="[('deprecated', '=', False)]",
+        help="This account will be used instead of the default one as "
+        "the income account for the current partner",
+    )
+
+    property_account_expense_id = fields.Many2one(
+        "account.account",
+        company_dependent=True,
+        string="Expense account",
+        oldname="default_expense_account",
+        domain="[('deprecated', '=', False)]",
+        help="This account will be used instead of the default one as "
+        "the expense account for the current partner",
+    )
