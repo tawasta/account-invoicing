594f996c9d380a1544c383ad87b6817d9f6b6fbe
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-10-26 14:05:53 +0300

    [IMP] Save picking priorities as char to prevent evaluating 0 as False

--- account_invoice_stock_picking_downpayment/models/res_config_settings.py
+++ account_invoice_stock_picking_downpayment/models/res_config_settings.py
@@ -1,17 +1,27 @@
+from odoo import api
 from odoo import fields, models
-
+from odoo.exceptions import ValidationError
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    stock_picking_waiting_payment_priority = fields.Integer(
+    stock_picking_waiting_payment_priority = fields.Char(
         string="Picking priority when waiting down payments",
         config_parameter="stock.picking.waiting.payment.priority",
-        default=0,
+        default="0",
     )
 
-    stock_picking_completed_payment_priority = fields.Integer(
+    stock_picking_completed_payment_priority = fields.Char(
         string="Picking priority after down payments",
         config_parameter="stock.picking.completed.payment.priority",
-        default=1,
+        default="1",
     )
+    @api.constrains('stock_picking_waiting_payment_priority', 'stock_picking_completed_payment_priority')
+    def _check_stock_picking_priority(self):
+        for record in self:
+            # TODO: check if the priority value actually exists
+            try:
+                int(record.stock_picking_waiting_payment_priority)
+                int(record.stock_picking_completed_payment_priority)
+            except ValueError:
+                raise ValidationError("Please use numeric values for picking priority")
