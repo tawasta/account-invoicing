7ec3dccb24a8ff6823ccf736ac1db3f768344eb6
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-10-28 09:30:31 +0300

    [MIG] 14.0 account_invoice_merge_never_merge_lines

---
+++ account_invoice_merge_never_merge_lines/models/account_move.py
@@ -0,0 +1,13 @@
+from odoo import models
+
+
+class AccountMove(models.Model):
+
+    _inherit = "account.move"
+
+    def _get_invoice_line_key_cols(self):
+        res = super()._get_invoice_line_key_cols()
+
+        res.append("id")
+
+        return res
