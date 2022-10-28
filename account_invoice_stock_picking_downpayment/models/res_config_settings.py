6642e0877a89b136146df2ffec7cbe3288da76a2
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-10-28 09:12:59 +0300

    [REF] Raising error

--- account_invoice_stock_picking_downpayment/models/res_config_settings.py
+++ account_invoice_stock_picking_downpayment/models/res_config_settings.py
@@ -1,6 +1,6 @@
-from odoo import api
-from odoo import fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
@@ -16,12 +16,18 @@ class ResConfigSettings(models.TransientModel):
         config_parameter="stock.picking.completed.payment.priority",
         default="1",
     )
-    @api.constrains('stock_picking_waiting_payment_priority', 'stock_picking_completed_payment_priority')
+
+    @api.constrains(
+        "stock_picking_waiting_payment_priority",
+        "stock_picking_completed_payment_priority",
+    )
     def _check_stock_picking_priority(self):
         for record in self:
             # TODO: check if the priority value actually exists
             try:
                 int(record.stock_picking_waiting_payment_priority)
                 int(record.stock_picking_completed_payment_priority)
-            except ValueError:
-                raise ValidationError("Please use numeric values for picking priority")
+            except ValueError as e:
+                raise ValidationError(
+                    _("Please use numeric values for picking priority")
+                ) from e
