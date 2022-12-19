55a158f9a28cee0b89ca800d47a186589f310eed
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-12-19 12:21:01 +0200

    [IMP] Partner in commission lines, refactored compute

--- account_invoice_commission_payment/__manifest__.py
+++ account_invoice_commission_payment/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Account Invoice - Create commission payments from invoices",
     "summary": "Allows Making commission payments from invoices",
-    "version": "14.0.1.6.6",
+    "version": "14.0.1.6.7",
     "category": "Invoicing",
     "website": "https://gitlab.com/tawasta/odoo/account-invoicing",
     "author": "Tawasta",
