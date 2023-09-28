6783fe853a0ff431d83827910784c0e1b16a62ef
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-09-28 15:39:05 +0300

    [FIX] Broken relation

--- account_invoice_vendor_invoice_to_sale/wizard/invoice_to_sale.py
+++ account_invoice_vendor_invoice_to_sale/wizard/invoice_to_sale.py
@@ -1,5 +1,3 @@
-import logging
-
 from odoo import _, fields, models
 from odoo.exceptions import UserError
 
@@ -163,13 +161,9 @@ class InvoiceToSale(models.TransientModel):
                 limit=1,
             )
 
-            logging.info("==MENEE TANNE JOSSA ETSITAAN ORDER=====")
-            logging.info(sale_order)
-
         if self.merge_order and sale_order:
             # Merge lines to an existing order
             sale_order.order_line = order_lines
-            logging.info("====KAY TAALLA======")
         else:
             # Create a new order
             values = dict(
@@ -183,12 +177,9 @@ class InvoiceToSale(models.TransientModel):
             sale_order = SaleOrder.create(
                 values,
             )
-            logging.info("====MENEE TANNE 1======")
-            logging.info(sale_order)
 
         # Auto-confirm order
         if self.auto_confirm and sale_order.state in ["draft", "sent"]:
-            logging.info("====MENEE TANNE======")
             sale_order.action_confirm()
 
         return {
