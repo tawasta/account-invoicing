b93e5e626cbe96fe378fa958240d9dc4331aac1c
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-10-02 13:50:33 +0300

    [IMP] Show invoice circulation only in supplier invoices

--- account_invoice_circulation/__manifest__.py
+++ account_invoice_circulation/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Invoice circulation",
     "summary": "Adds invoice circulation for vendor invoices",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Invoicing",
     "website": "https://gitlab.com/tawasta/odoo/account-invoicing",
     "author": "Tawasta",
