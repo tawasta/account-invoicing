52f97c1fbfe434814c2a95e9f900ac3f14088fd4
difbit <timo.kekalainen@tawasta.fi>
2024-01-11 14:18:56 +0200

    [ADD] A new 17.0 branch for Odoo version 17.0

--- account_invoice_commission_payment/models/config_settings.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-
-    commission_communication = fields.Char(
-        related="company_id.commission_communication", readonly=False
-    )
