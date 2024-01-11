9fcba16eabf63d1486010839aaaf9e76300d572e
difbit <timo.kekalainen@tawasta.fi>
2024-01-11 15:16:30 +0200

    [MIG] Migration of account_invoice_overdue_interest module from 14.0 to 17.0 version

---
+++ account_invoice_overdue_interest/__manifest__.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2018 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Overdue Interest for Invoices",
+    "summary": "Overdue interest % field for invoices and partners",
+    "version": "17.0.1.0.0",
+    "category": "Invoicing",
+    "website": "https://gitlab.com/tawasta/odoo/account-invoicing",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["account", "sale"],
+    "data": [
+        "views/account_move.xml",
+        "views/report_invoice.xml",
+        "views/res_config_settings.xml",
+        "views/res_partner.xml",
+    ],
+    "demo": [],
+}
---
+++ account_invoice_overdue_interest/models/sale_order.py
@@ -0,0 +1,16 @@
+from odoo import models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    def _prepare_invoice(self):
+        # Handling for when invoicing the invoiceable lines
+        self.ensure_one()
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals["overdue_interest"] = (
+            self.partner_id.overdue_interest or self.company_id.overdue_interest
+        )
+
+        return invoice_vals
