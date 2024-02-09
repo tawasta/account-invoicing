6b976cf9a5c3048ce7635814c7347920f0c1d596
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-02-09 07:47:29 +0200

    Modified function

--- account_move_payment_link/models/account_move.py
+++ account_move_payment_link/models/account_move.py
@@ -10,17 +10,21 @@ class AccountMove(models.Model):
     def _compute_payment_link(self):
         payment_link_wizard = self.env["payment.link.wizard"]
         for rec in self:
-            logging.info("======REC ARVO====");
-            logging.info(rec);
-            temp_wizard = payment_link_wizard.create(
-                {
-                    "res_model": "account.move",
-                    "res_id": rec.id,
-                    "amount": rec.amount_total,
-                    "partner_id": rec.partner_id.id,
-                    "currency_id": rec.currency_id.id,
-                    "description": rec.payment_reference,
-                }
-            )
-            temp_wizard._compute_values()
-            rec.link = temp_wizard.link
+            record = self.env["account.move"].browse(rec)
+            if record:
+                logging.info("======REC ARVO====");
+                logging.info(rec);
+                temp_wizard = payment_link_wizard.create(
+                    {
+                        "res_model": "account.move",
+                        "res_id": rec.id,
+                        "amount": rec.amount_total,
+                        "partner_id": rec.partner_id.id,
+                        "currency_id": rec.currency_id.id,
+                        "description": rec.payment_reference,
+                    }
+                )
+                temp_wizard._compute_values()
+                rec.link = temp_wizard.link
+            else:
+                rec.link = False
