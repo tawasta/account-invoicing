6de141821f589be517878bdfba7a2d480526dba6
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-02-09 07:54:49 +0200

    Modified function

--- account_move_payment_link/models/account_move.py
+++ account_move_payment_link/models/account_move.py
@@ -10,21 +10,21 @@ class AccountMove(models.Model):
     def _compute_payment_link(self):
         payment_link_wizard = self.env["payment.link.wizard"]
         for rec in self:
-        # Tarkista onko rec.id todellinen tietueen ID (ei NewId-objekti)
-        if isinstance(rec.id, int):
-            logging.info("======REC ARVO====")
-            logging.info(rec)
-            temp_wizard = payment_link_wizard.create(
-                {
-                    "res_model": "account.move",
-                    "res_id": rec.id,
-                    "amount": rec.amount_total,
-                    "partner_id": rec.partner_id.id,
-                    "currency_id": rec.currency_id.id,
-                    "description": rec.payment_reference,
-                }
-            )
-            temp_wizard._compute_values()
-            rec.link = temp_wizard.link
-        else:
-            rec.link = False
+            # Tarkista onko rec.id todellinen tietueen ID (ei NewId-objekti)
+            if isinstance(rec.id, int):
+                logging.info("======REC ARVO====")
+                logging.info(rec)
+                temp_wizard = payment_link_wizard.create(
+                    {
+                        "res_model": "account.move",
+                        "res_id": rec.id,
+                        "amount": rec.amount_total,
+                        "partner_id": rec.partner_id.id,
+                        "currency_id": rec.currency_id.id,
+                        "description": rec.payment_reference,
+                    }
+                )
+                temp_wizard._compute_values()
+                rec.link = temp_wizard.link
+            else:
+                rec.link = False
