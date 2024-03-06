4205d3578830321ea38aa9005b4a31a0ec12564d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-03-06 14:38:43 +0200

    [IMP] Better permission group

--- account_invoice_quick_post/__manifest__.py
+++ account_invoice_quick_post/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "A quick post button for invoice line view",
     "summary": "Post (confirm) invoices from line view",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Invoicing",
     "website": "https://gitlab.com/tawasta/odoo/account-invoicing",
     "author": "Tawasta",
