2ba9b3fd2bacfec50b7c3bab3e9b8922432722e3
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-03-28 13:11:14 +0200

    Created new module

---
+++ account_credit_control_sales_contact/models/credit_control_line.py
@@ -0,0 +1,19 @@
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class CreditControlLine(models.Model):
+
+    _inherit = "credit.control.line"
+
+    sales_agent = fields.Many2one(
+        string="Sales contact",
+        comodel_name="sales.agent",
+        compute="_compute_sales_agent_id",
+        store=True,
+    )
+
+    @api.depends("partner_id.sales_agent")
+    def _compute_sales_agent_id(self):
+        for line in self:
+            line.sales_agent = line.partner_id.sales_agent
