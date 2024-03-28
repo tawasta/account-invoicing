caa1b2291c62727cda9bee3652a2b7b9ad69b0c2
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-03-28 16:10:11 +0200

    [IMP] Also replace product name, when display name is not set

--- account_invoice_line_label_without_product/models/account_move_line.py
+++ account_invoice_line_label_without_product/models/account_move_line.py
@@ -10,6 +10,10 @@ class AccountMoveLine(models.Model):
         # Replace product display name from line name
         res = res.replace(self.product_id.display_name, "")
 
+        # Replace product name from line name
+        # (For cases where name is used instead of display name)
+        res = res.replace(self.product_id.name, "")
+
         if not res:
             # Empty name is not allowed
             res = " "
