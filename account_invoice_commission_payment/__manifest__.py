65ca711f1df419d2aacbbf866823f93f35afa929
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-11-07 10:16:24 +0200

    [IMP] Show cost total instead of unit price

--- account_invoice_commission_payment/__manifest__.py
+++ account_invoice_commission_payment/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Account Invoice Commission - Create commission payments from invoices",
     "summary": "Allows Making commission payments from invoices",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Invoicing",
     "website": "https://gitlab.com/tawasta/odoo/account-invoicing",
     "author": "Tawasta",
