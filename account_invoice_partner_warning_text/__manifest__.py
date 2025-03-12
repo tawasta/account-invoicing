6e638523586a1f5ac7f2447fcc6369b08c5c59a7
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-03-12 08:20:55 +0200

    [MIG] - Modified module to 17 version

--- account_invoice_partner_warning_text/__manifest__.py
+++ account_invoice_partner_warning_text/__manifest__.py
@@ -23,7 +23,7 @@
     "summary": "Adds Partner warning text to invoice",
     "version": "17.0.1.0.0",
     "category": "Account",
-    "website": "https://gitlab.com/tawasta/odoo/account-invoicing",
+    "website": "https://github.com/tawasta/account-invoicing",
     "author": "Tawasta",
     "license": "AGPL-3",
     "application": False,
