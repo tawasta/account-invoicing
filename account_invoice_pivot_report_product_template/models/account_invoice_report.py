abba69f43ad91e6f1d8b36027b9fac614621638c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-03-17 08:51:52 +0200

    [IMP] - modified module to 17 version

---
+++ account_invoice_pivot_report_product_template/models/account_invoice_report.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+
+class AccountInvoiceReport(models.Model):
+    _inherit = "account.invoice.report"
+
+    product_template_id = fields.Many2one(
+        "product.template", "Product Template", readonly=True
+    )
+
+    def _select(self):
+        return (
+            super()._select()
+            + ", product.product_tmpl_id as product_template_id"
+        )
