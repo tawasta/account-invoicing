d08a224afbedd4db51e2dec6119df58aa5fe7f2a
difbit <timo.kekalainen@tawasta.fi>
2025-07-24 13:53:31 +0300

    [IMP] account_invoice_default_payment_term module: Use company dependent settings

--- account_invoice_default_payment_term/models/account_move.py
+++ account_invoice_default_payment_term/models/account_move.py
@@ -6,18 +6,11 @@ class AccountMove(models.Model):
 
     @api.depends("partner_id")
     def _compute_invoice_payment_term_id(self):
-        default_payment_term_id = int(
-            self.env["ir.config_parameter"]
-            .sudo()
-            .get_param("account_invoice_default_payment_term.default_term")
-        )
-        default_payment_term = self.env["account.payment.term"].browse(
-            default_payment_term_id
-        )
-
         super()._compute_invoice_payment_term_id()
 
         for move in self:
+            default_payment_term = move.company_id.invoice_payment_term
+
             if (
                 not move.invoice_payment_term_id
                 and default_payment_term
--- account_invoice_default_payment_term/models/res_company.py
+++ account_invoice_default_payment_term/models/res_company.py
@@ -1,12 +1,10 @@
 from odoo import fields, models
 
 
-class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
+class ResCompany(models.Model):
+    _inherit = "res.company"
 
     invoice_payment_term = fields.Many2one(
         comodel_name="account.payment.term",
         string="Default Payment term",
-        related="company_id.invoice_payment_term",
-        readonly=False,
     )
