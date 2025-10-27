a0864ebc2b168487b94272da5d102140ba813e58
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-10-27 10:16:39 +0200

    [IMP] - created new module

--- README.md
+++ README.md
@@ -74,6 +74,7 @@ addon | version | maintainers | summary
 [account_invoice_vendor_invoice_purchase_representative](account_invoice_vendor_invoice_purchase_representative/) | 17.0.1.0.0 |  | Purchase Representative of Vendor Bill
 [account_invoice_vendor_invoice_to_sale](account_invoice_vendor_invoice_to_sale/) | 17.0.1.0.0 |  | Adds a wizards for creating a SO from vendor invoice
 [account_lock_date_update_group](account_lock_date_update_group/) | 17.0.1.0.0 |  | This module adds a group for locking account move dates
+[account_move_line_delete_multiple](account_move_line_delete_multiple/) | 17.0.1.0.0 |  | Vendor Bills: Delete lines with checkboxes
 [account_move_payment_link](account_move_payment_link/) | 17.0.1.0.3 |  | Autogenerates a payment link that can embedded into an email template
 [account_move_uppercase_first_order](account_move_uppercase_first_order/) | 17.0.1.0.0 |  | Account move: Uppercase-first alphabetical order
 [account_payment_commission_send_email](account_payment_commission_send_email/) | 17.0.1.0.2 |  | Allows sending a payment summary by email
--- account_move_line_delete_multiple/models/account_move_line.py
+++ account_move_line_delete_multiple/models/account_move_line.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 import logging
+
 from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
 
-ALLOWED_TYPES = ('in_invoice', 'in_refund', 'in_receipt')
-ALLOWED_DISPLAY = ('product', 'line_section', 'line_note')
+ALLOWED_TYPES = ("in_invoice", "in_refund", "in_receipt")
+ALLOWED_DISPLAY = ("product", "line_section", "line_note")
 
 
 class AccountMoveLine(models.Model):
@@ -19,12 +19,12 @@ class AccountMoveLine(models.Model):
     def write(self, vals):
         res = super().write(vals)
 
-        if vals.get('mark_for_delete'):
+        if vals.get("mark_for_delete"):
             deletables = self.filtered(
-                lambda l: l.move_id
-                and l.move_id.state == 'draft'
-                and l.move_id.move_type in ALLOWED_TYPES
-                and l.display_type in ALLOWED_DISPLAY
+                lambda li: li.move_id
+                and li.move_id.state == "draft"
+                and li.move_id.move_type in ALLOWED_TYPES
+                and li.display_type in ALLOWED_DISPLAY
             )
             if deletables:
                 deletables.unlink()
