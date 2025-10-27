2b7870a2e21f99dbee0f7cd34ff12391b188359d
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-10-27 16:03:00 +0200

    [IMP] - create new module

--- account_move_line_delete_multiple/models/account_move_line.py
+++ account_move_line_delete_multiple/models/account_move_line.py
@@ -1,11 +1,10 @@
 import logging
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
-ALLOWED_TYPES = ("in_invoice", "in_refund", "in_receipt")
-ALLOWED_DISPLAY = ("product", "line_section", "line_note")
+ALLOWED_TYPES = {"in_invoice", "in_refund", "in_receipt"}
+ALLOWED_DISPLAY = {"product", "line_section", "line_note"}
 
 
 class AccountMoveLine(models.Model):
@@ -16,16 +15,22 @@ class AccountMoveLine(models.Model):
         help="Tick to delete this line (draft vendor bills only).",
     )
 
-    def write(self, vals):
-        res = super().write(vals)
-
-        if vals.get("mark_for_delete"):
-            deletables = self.filtered(
-                lambda li: li.move_id
+    def _eligible_for_manual_delete(self):
+        """Return only draft vendor bill lines that can be safely deleted."""
+        return self.filtered(
+            lambda li: (
+                li.move_id
                 and li.move_id.state == "draft"
                 and li.move_id.move_type in ALLOWED_TYPES
                 and li.display_type in ALLOWED_DISPLAY
+                and li in li.move_id.invoice_line_ids
             )
+        )
+
+    def write(self, vals):
+        res = super().write(vals)
+        if vals.get("mark_for_delete"):
+            deletables = self._eligible_for_manual_delete()
             if deletables:
                 deletables.unlink()
         return res
