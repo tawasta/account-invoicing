93e94a73fab974eb206ce349065f949666ce58e0
Jarmo Kortetjärvi <jarmo.kortetjarvi@futural.fi>
2026-01-21 16:27:39 +0200

    [MIG] account_invoice_commission: Migration to 19.0

--- account_invoice_commission/__manifest__.py
+++ account_invoice_commission/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Account Invoice Commission",
     "summary": "Add a 'commission paid' field for invoices and invoice lines",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Invoicing",
     "website": "https://github.com/tawasta/account-invoicing",
     "author": "Futural",
