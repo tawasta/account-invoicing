d88df3df70c0f9be6b20ef02c1e6918d27fe272c
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-01-26 12:08:43 +0200

    [MIG] 19.0 acocunt_invoice_overdue_interest

---
+++ account_invoice_overdue_interest/models/account_move.py
@@ -0,0 +1,21 @@
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    overdue_interest = fields.Float(
+        string="Overdue interest %",
+        digits=(4, 2),
+        compute="_compute_overdue_interest",
+        store=True,
+        readonly=False,
+        precompute=True,
+    )
+
+    @api.depends("partner_id")
+    def _compute_overdue_interest(self):
+        for record in self:
+            record.overdue_interest = (
+                record.partner_id.overdue_interest or record.company_id.overdue_interest
+            )
---
+++ account_invoice_overdue_interest/models/res_partner.py
@@ -0,0 +1,22 @@
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    overdue_interest = fields.Float(
+        string="Overdue interest %",
+        digits=(4, 2),
+        help="Use this overdue interest for invoices. "
+        "Leave at zero to use company default.",
+    )
+
+    @api.model
+    def _commercial_fields(self):
+        """Returns the list of fields that are managed by the commercial entity
+        to which a partner belongs. These fields are meant to be hidden on
+        partners that aren't `commercial entities` themselves, and will be
+        delegated to the parent `commercial entity`. The list is meant to be
+        extended by inheriting classes."""
+        commercial_fields = super()._commercial_fields()
+        return commercial_fields + ["overdue_interest"]
