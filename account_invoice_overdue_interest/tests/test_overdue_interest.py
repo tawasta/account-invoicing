1ba13642be1c9029855796e5605fe54e0b8ec529
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-01-26 15:24:23 +0200

    Getting tests to run

--- account_invoice_overdue_interest/tests/test_overdue_interest.py
+++ account_invoice_overdue_interest/tests/test_overdue_interest.py
@@ -8,7 +8,7 @@ class TestOverdueInterest(TransactionCase):
         super().setUp()
 
         # Set default overdue interest
-        #self.env["res.company"].search([]).write({"overdue_interest": 11.5})
+        self.env["res.company"].search([]).write({"overdue_interest": 11.5})
 
         self.ResPartner = self.env["res.partner"]
 
@@ -17,14 +17,14 @@ class TestOverdueInterest(TransactionCase):
             dict(name="Yritys Oy", overdue_interest="10.0")
         )
 
-        #self.partner2 = self.ResPartner.create(dict(name="Firma Oy"))
+        self.partner2 = self.ResPartner.create(dict(name="Firma Oy"))
 
     def test_overdue_interest(self):
         inv = self.env["account.move"].create({"partner_id": self.partner1.id})
         # This should use the partner-spesific overdue interest
         self.assertEqual(inv.overdue_interest, 10.0)
 
-    #def test_default_overdue_interest(self):
-        #inv = self.env["account.move"].create({"partner_id": self.partner2.id})
+    def test_default_overdue_interest(self):
+        inv = self.env["account.move"].create({"partner_id": self.partner2.id})
         # This should use the default overdue interest from company
-        #self.assertEqual(inv.overdue_interest, 11.5)
+        self.assertEqual(inv.overdue_interest, 11.5)
