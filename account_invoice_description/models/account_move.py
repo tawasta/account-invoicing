65ffa4c564ed506a9d273c47141927baa11c8a1c
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-07-21 12:35:49 +0300

    [MIG] 19.0 account_invoice_description (#184)

---
+++ account_invoice_description/models/account_move.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    description = fields.Text(
+        string="Internal note",
+        help="Not shown to partner.",
+    )
